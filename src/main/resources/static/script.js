a116298174296e8035de6e450ea80c3a90e56a0d
Vinicius <viniciusribeiro0709@gmail.com>
2026-07-05 20:38:21 -0300

    Colocando novas alterações

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,9 @@
+function fecharMenu() {
+    menulateral = document.getElementById("menuLateral");
+    menulateral.style.display = "none";
+}
+
+function AbrirMenu() {
+    menulateral = document.getElementById("menuLateral");
+    menulateral.style.display = "flex";
+}
--- src/test/java/com/example/Livraria/LivrariaApplicationTests.java
+++ src/test/java/com/example/Livraria/LivrariaApplicationTests.java
@@ -9,5 +9,6 @@ class LivrariaApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	
 
 }
